727f08886abe151f6079981e38b97ec9d2c9c225
ingwon97 <ghkdrnjs180@naver.com>
2022-08-22 17:19:36 +0900

    여러대의 채팅서버간에 메시지 공유하기 by Redis pub/sub

--- src/main/java/com/clone/kakaoclone/websocket/ChatRoom.java
+++ src/main/java/com/clone/kakaoclone/websocket/ChatRoom.java
@@ -2,14 +2,18 @@ package com.clone.kakaoclone.websocket;
 
 import lombok.*;
 
+import java.io.Serializable;
 import java.util.UUID;
 
 @Getter
 @Setter
-@NoArgsConstructor
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
-public class ChatRoom {
+public class ChatRoom implements Serializable {
+
+    private static final long serialVersionUID = 6494678977089006639L;
+
     private String roomId;
     private String name;
 
