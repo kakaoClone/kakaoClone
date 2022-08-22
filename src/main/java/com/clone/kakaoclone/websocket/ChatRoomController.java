6f569c47205013c83dc0f58ef3553f7e581643a4
ingwon97 <ghkdrnjs180@naver.com>
2022-08-22 18:54:02 +0900

    web socket + friendController 수정

--- src/main/java/com/clone/kakaoclone/websocket/ChatRoomController.java
+++ src/main/java/com/clone/kakaoclone/websocket/ChatRoomController.java
@@ -19,8 +19,9 @@ public class ChatRoomController {
     // 채팅 리스트 화면
     @GetMapping("/room")
     public String rooms(Model model) {
-        return "/chat/room";
+        return "chat/room";
     }
+
     // 모든 채팅방 목록 반환
     @GetMapping("/rooms")
     @ResponseBody
@@ -33,11 +34,12 @@ public class ChatRoomController {
     public ChatRoom createRoom(@RequestParam String name) {
         return chatRoomRepository.createChatRoom(name);
     }
+
     // 채팅방 입장 화면
     @GetMapping("/room/enter/{roomId}")
     public String roomDetail(Model model, @PathVariable String roomId) {
         model.addAttribute("roomId", roomId);
-        return "/chat/roomdetail";
+        return "chat/roomdetail";
     }
     // 특정 채팅방 조회
     @GetMapping("/room/{roomId}")
